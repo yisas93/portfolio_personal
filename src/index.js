0d5a4c6f7f66fd9dc9cae059a047fe36d9e051fb
yisas93 <juan249325@gmail.com>
2024-01-09 23:19:24 +0000

    blank page troubleshoot

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <React.StrictMode basename={process.env.PUBLIC_URL}>
     <App />
   </React.StrictMode>
 );
